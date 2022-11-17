502882f713c1eadc3d2d9c0c3b0bf07a4af5003b
EduardoAmorim2 <eduardoamorim855@gmail.com>
2022-11-16 23:51:52 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:crud/models/users.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Eduardo',
+    email: 'eduardoamorim855@gmail.com',
+    avatarUrl: 'https://github.com/EduardoAmorim2.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Camila',
+    email: 'camilareinehr@gmail.com',
+    avatarUrl: 'https://github.com/EduardoAmorim2.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Henrique',
+    email: 'henriqueamorim@gmail.com',
+    avatarUrl: 'https://github.com/EduardoAmorim2.png',
+  ),
+};
